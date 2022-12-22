0dfc1f133c27c5191447a0759ea349154dbc7be7
JoachimDieu <dieu.joachim@gmail.com>
2022-12-22 23:48:00 +0100

    Remarque Elea

--- src/Page/Accueil/Accueil.js
+++ src/Page/Accueil/Accueil.js
@@ -103,7 +103,7 @@ export default function Accueil(){
             <p className="gs_reveal">L'année dernière, les Bureaux Des Sports d'HEI, ISA et ISEN sont réunis pour ne faire qu'une AS JUNIA. C'est une nouvelle aventure qui permettra aux étudiants du groupe Junia de se rencontrer autour du sport.</p>
             <br></br>
             <br></br>
-            <p className="gs_reveal">L'équipe Univok, 38 étudiants de Junia, a gagné les campagnes en mai 2022 et est devenus la nouvelle AS JUNIA.</p>
+            <p className="gs_reveal">L'équipe Univok de Junia, a gagné les campagnes en mai 2022 et est devenus la nouvelle AS JUNIA.</p>
             <br></br>
             <br></br>
             <p className="gs_reveal">Pour l'année 2022-2023, l'AS JUNIA compte 52 associations sportives qui proposent des sports individuels et collectifs variés (badminton, danse, rugby, golf...).</p>
@@ -124,7 +124,7 @@ export default function Accueil(){
 
 
         <div className="infoMembre gs_reveal">
-          Nous sommes une équipe de 38 étudiants de Junia de différentes promotions. <br></br>Pour mieux nous organiser, nous nous sommes répartis dans plusieurs pôles, en fonction de ce qui correspond le mieux à chacun.
+          Nous sommes une équipe de 39 étudiants de Junia de différentes promotions. <br></br>Pour mieux nous organiser, nous nous sommes répartis dans plusieurs pôles, en fonction de ce qui correspond le mieux à chacun.
           <br></br>Tu trouveras ci-dessous les membres de chaque pôle.</div>
 
         {/* Bureau */}
